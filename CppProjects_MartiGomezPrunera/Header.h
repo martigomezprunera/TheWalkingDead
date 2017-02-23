c8ae9361978bc84f3670c91890e3582e48e9c937
martigomezprunera <martigomezprunera@S3-33>
2017-02-23 12:56:30 +0100

    Funciones

--- CppProjects_MartiGomezPrunera/Header.h
+++ CppProjects_MartiGomezPrunera/Header.h
@@ -5,16 +5,77 @@
 //Enumeracion fuerte
 enum class Weapon {FISTS, GUN, SHOTGUN, REVOLVER, SNIPER, MACHINE_GUN, MAX };
 
+class Zombie; //Forward Declaration
 class Player
 {
+public:
+	Weapon weapon;
 	float precision;
 	int life;
+	Player();
+
+private:
+	void attack(Zombie &Z)
+	{
+		int damage;
+		switch (weapon)
+		{
+		case Weapon::FISTS:
+			damage = 0 * precision;
+			break;
+		case Weapon::GUN:
+			damage = 1 * precision;
+			break;
+		case Weapon::SHOTGUN:
+			damage = 2 * precision;
+			break;
+		case Weapon::REVOLVER:
+			damage = 3 * precision;
+			 
+			break;
+		case Weapon::SNIPER:
+			damage = 4 * precision;
+			break;
+		case Weapon::MACHINE_GUN:
+			damage = 5 * precision;
+			break;
+		default:
+			break;
+		}
+		Z.life = Z.life - damage;
+	}
+	bool isAlive()
+	{
+
+	}
 };
+
+Player::Player()
+{
+}
+
 
 class Zombie
 {
-	int distanceToPlayer;
+public:
+	int disntanceToPlayer;
 	float speed;
 	float damage;
-	int life = 100;
+	int life;
+	Zombie();
+
+private:
+	void attack(Player &P)
+	{
+
+	}
+	bool isAlive()
+	{
+
+	}
 };
+
+Zombie::Zombie()
+{
+}
+
---
+++ CppProjects_MartiGomezPrunera/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include "Header.h"
+
+int main()
+{
+	srand(static_cast<unsigned> (time(nullptr));
+}
